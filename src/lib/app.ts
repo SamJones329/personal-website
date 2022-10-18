fc25f118e73bd2023ef14409d2569475f8fc698b
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-10-18 01:03:25 -0500

    changed over to svelte kit with tailwindcss

---
+++ src/lib/app.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, isSupported } from 'firebase/analytics';
+import type { FirebaseApp, FirebaseOptions } from "firebase/app";
+import type { Analytics } from "firebase/analytics";
+// import { dev } from '$app/environment' // browser
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyAhVzIbyS8zG1Su0xTTZ1jbCqLkAO_HMm0",
+  authDomain: "personal-website-46a49.firebaseapp.com",
+  projectId: "personal-website-46a49",
+  storageBucket: "personal-website-46a49.appspot.com",
+  messagingSenderId: "258594875840",
+  appId: "1:258594875840:web:c7217f867527bb70ad8025",
+  measurementId: "G-XQ3SX9PY6P"
+};
+
+const app: FirebaseApp = initializeApp(firebaseConfig);
+let analytics: Analytics;
+isSupported().then((analyticsSupported) => {
+  if(analyticsSupported) analytics = getAnalytics(app);
+});
+
+// export default app;
+export { app, analytics };
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
---
+++ src/routes/huh/+page.svelte
@@ -0,0 +1,11 @@
+<style>
+  video {
+    margin: 10rem auto;
+  }
+</style>
+
+<video width="320" height="240" controls>
+  <source src="huh.mp4" type="video/mp4">
+  <track kind=captions label=English srclang=en src="huh.vtt">
+  Your browser does not support the video tag.
+</video> 
